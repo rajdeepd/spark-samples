996418b02928b50d004003cb7b23db90fad8b39b
Rajdeep Dua <dua_rajdeep@yahoo.com>
2015-04-20 20:13:47 -0700

    First Set of samples spark

---
+++ ml-samples/src/main/scala/HashingTFSample.scala
@@ -0,0 +1,20 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.feature.HashingTF
+import org.apache.spark.mllib.classification.LogisticRegressionWithSGD
+
+object HashingTFSample{
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("SpamFilter")
+    val sc = new SparkContext(conf)
+    val sentence = "This is a simple text"
+    val words = sentence.split(" ")
+    val tf = new HashingTF(numFeatures = 10000)
+    val sparseVector = words.map(w => tf.transform(w))
+    for(sv <- sparseVector) {
+      println(s"$sv")
+    }
+  }
+}
---
+++ ml-samples/src/main/scala/VectorsSample.scala
@@ -0,0 +1,17 @@
+/**
+ * @author ubuntu
+ */
+import org.apache.spark.mllib.linalg.Vectors
+
+object VectorsSample {
+  def main(args: Array[String]) {
+    // Create the dense vector <1.0, 2.0, 3.0>; Vectors.dense takes values or an array
+    val denseVec1 = Vectors.dense(1.0, 2.0, 3.0)
+    val denseVec2 = Vectors.dense(Array(1.0, 2.0, 3.0))
+    println(denseVec1)
+    println(denseVec2)
+    // Create the sparse vector <1.0, 0.0, 2.0, 0.0>; Vectors.sparse takes the size of
+    // the vector (here 4) and the positions and values of nonzero entries
+    val sparseVec1 = Vectors.sparse(4, Array(0, 2), Array(1.0, 2.0))
+  }
+}
