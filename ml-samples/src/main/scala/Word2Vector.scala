df76e347cb986c1a27e4ff8cf1b1f17223590d40
Rajdeep Dua <dua_rajdeep@yahoo.com>
2015-05-07 03:41:54 -0700

    Added new samples for Machine Learning Module

---
+++ ml-samples/src/main/scala/Word2Vector.scala
@@ -0,0 +1,21 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.feature.Word2Vec
+import org.apache.spark.rdd.RDD
+
+object Word2Vector{
+  def main(args: Array[String]) {
+    val file = "/home/ubuntu/work/spark/spark-samples/ml-samples/data/text8_10000"
+    val conf = new SparkConf().setAppName("Word2Vector")
+    val sc = new SparkContext(conf)
+    val input = sc.textFile(file).map(line => line.split(" ").toSeq)
+    val word2vec = new Word2Vec()
+    val model = word2vec.fit(input)
+    val synonyms = model.findSynonyms("anarchist", 40)
+    for((synonym, cosineSimilarity) <- synonyms) {
+      println(s"$synonym $cosineSimilarity")
+    }
+  }
+}
